b2a8aee8d903c6305721896bd906fde7d9bdc6c0
ademariocarvalho <ademariocarvalho@yahoo.com.br>
2021-06-21 14:19:43 -0300

    primeiro commit

---
+++ dprPhoton.dpr
@@ -0,0 +1,14 @@
+program dprPhoton;
+
+uses
+  Vcl.Forms,
+  uPhoton in 'uPhoton.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
